9242579daea21a78484660dea469dc3b45902d0e
Vyas Ramasubramani <vyasr@nvidia.com>
2023-06-22 19:21:16 -0700

    Clean up build script

--- demo.sh
+++ demo.sh
@@ -1,9 +1,8 @@
-mkdir /code/cpp/build
-pushd /code/cpp/build
-cmake ..
-make
-make install
-popd
+cmake -B /code/cpp/build -S /code/cpp
+cmake --build /code/cpp/build
+cmake --install /code/cpp/build
+rm -rf /code/cpp/build
+ldconfig
 
 python3 -m pip install ./code/pysaxpy
 python3 demo.py
