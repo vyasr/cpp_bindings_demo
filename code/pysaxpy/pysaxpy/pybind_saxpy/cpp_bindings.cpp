606682a841867e5ee7bd2a7b5c878146d4a1cc3e
Vyas Ramasubramani <vyasr@nvidia.com>
2023-06-23 07:43:03 -0700

    Fix pybind bindings

--- code/pysaxpy/pysaxpy/pybind_saxpy/cpp_bindings.cpp
+++ code/pysaxpy/pysaxpy/pybind_saxpy/cpp_bindings.cpp
@@ -4,5 +4,7 @@
 PYBIND11_MODULE(cpp_bindings, m) {
     m.doc() = "pybind11 C++ bindings"; // optional module docstring
 
-    m.def("saxpy", &saxpy, "Compute a * x + y");
+    m.def("saxpy", [](float a, long x_begin, long x_end, long y_begin, long output_begin){
+            saxpy(a, reinterpret_cast<float const*>(x_begin), reinterpret_cast<float const*>(x_end), reinterpret_cast<float const*>(y_begin), reinterpret_cast<float *>(output_begin));
+            }, "Compute a * x + y");
 }
