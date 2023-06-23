b0df9cf9a11182ece03f805db4e0d667e70ae32b
Vyas Ramasubramani <vyasr@nvidia.com>
2023-06-23 07:22:38 -0700

    Add a header-only version of the library

---
+++ code/cpp/include_only/saxpy_header_only.hpp
@@ -0,0 +1,17 @@
+#include <thrust/execution_policy.h>
+#include <thrust/transform.h>
+
+template <typename T>
+struct SaxpyFunctor {
+    SaxpyFunctor(T const a) : a(a) {}
+    T operator()(const T x, const T y) const {
+        return a * x + y;
+    }
+
+    T const a;
+};
+
+template <typename Scalar, typename InputIterator, typename OutputIterator>
+void saxpy(Scalar a, InputIterator x_begin, InputIterator x_end, InputIterator y_begin, OutputIterator output_begin) {
+    thrust::transform(thrust::device, x_begin, x_end, y_begin, output_begin, SaxpyFunctor{a});
+}
